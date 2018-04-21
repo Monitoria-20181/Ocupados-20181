69b0f0254de97c61a7a24ba5b1e9bf74a683d122
Juan David Ramirez Mendoza <juan.ramirez-me@mail.escuelaing.edu.co>
2018-04-21 10:29:05 -0500

    Procedure statement

--- src/main/java/edu/eci/persistencia/base.java
+++ src/main/java/edu/eci/persistencia/base.java
@@ -5,7 +5,6 @@
  */
 package edu.eci.persistencia;
 
-
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.sql.CallableStatement;
@@ -26,19 +25,19 @@ import java.util.logging.Logger;
  * @author Contabilidad
  */
 public class base {
- 
-    Connection con ;
+
+    Connection con;
     Statement stmt = null;
     ResultSet rs = null;
-    
-    public base ()  {
-       
+
+    public base() {
+
     }
-    
-    public ArrayList<String> ejecutarSP(          ) {
+
+    public ArrayList<String> ejecutarSP() {
         ArrayList<String> equipos = new ArrayList<>();
         try {
-            
+
             Properties propiedades = new Properties();
             InputStream entrada = null;
             String servidor;
@@ -47,56 +46,55 @@ public class base {
             String user;
             String password;
 
-            
+            entrada = new FileInputStream("Scripts\\conexion.txt");
+            propiedades.load(entrada);
 
-       entrada = new FileInputStream("Scripts\\conexion.txt");
-       propiedades.load(entrada);
-       
-       servidor = propiedades.getProperty("servidor");
-       puerto = propiedades.getProperty("puerto");
-       namebd = propiedades.getProperty("namebd");
-       user = propiedades.getProperty("user");
-       password = propiedades.getProperty("password");
-       
-       System.out.println("jdbc:mysql://"+servidor+":"+puerto+"/"+namebd+"?user="+user+"&password="+password);
+            servidor = propiedades.getProperty("servidor");
+            puerto = propiedades.getProperty("puerto");
+            namebd = propiedades.getProperty("namebd");
+            user = propiedades.getProperty("user");
+            password = propiedades.getProperty("password");
 
-            
-            
-           Class.forName("com.mysql.jdbc.Driver");
-           con = DriverManager.getConnection("jdbc:mysql://"+servidor+":"+puerto+"/"+namebd+"?user="+user+"&password="+password);
-             //con = DriverManager.getConnection("jdbc:mysql://laboratorio.is.escuelaing.edu.co/control?user=controlr&password=PC-Libres@");
-           
-      
-          
-          Calendar c1 = Calendar.getInstance(); 
-          String dia = Integer.toString(c1.get(Calendar.DATE));
-          String mes = Integer.toString(c1.get(Calendar.MONTH));
-          
-          System.out.println(mes);
-          String annio = Integer.toString(c1.get(Calendar.YEAR));
-          int hora= c1.get(Calendar.HOUR_OF_DAY);
-          int minutos= c1.get(Calendar.MINUTE);
-          int segundos = c1.get(Calendar.SECOND);
-          String hora1 = "";
-          hora1 = String.valueOf(hora);
-          String minutos1= "";
-          minutos1 = String.valueOf(minutos);
-          String segundos1 = "";
-          segundos1 = String.valueOf(segundos);
-  
-            //Statement stmt = con.createStatement();
+            System.out.println("jdbc:mysql://" + servidor + ":" + puerto + "/" + namebd + "?user=" + user + "&password=" + password);
+
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://" + servidor + ":" + puerto + "/" + namebd + "?user=" + user + "&password=" + password);
+            //con = DriverManager.getConnection("jdbc:mysql://laboratorio.is.escuelaing.edu.co/control?user=controlr&password=PC-Libres@");
+
+            Calendar c1 = Calendar.getInstance();
+            String dia = Integer.toString(c1.get(Calendar.DATE));
+            String mes = Integer.toString(c1.get(Calendar.MONTH));
+
+            System.out.println(mes);
+            String annio = Integer.toString(c1.get(Calendar.YEAR));
+            int hora = c1.get(Calendar.HOUR_OF_DAY);
+            int minutos = c1.get(Calendar.MINUTE);
+            int segundos = c1.get(Calendar.SECOND);
+            String hora1 = "";
+            hora1 = String.valueOf(hora);
+            String minutos1 = "";
+            minutos1 = String.valueOf(minutos);
+            String segundos1 = "";
+            segundos1 = String.valueOf(segundos);
+
+            Statement stmt = con.createStatement();
+            /*stmt.execute("CREATE DEFINER=`control`@`localhost` PROCEDURE `SP_GETEQUIPOSOCUPADOS`(fechaConsulta DATETIME)\n"
+                    + "BEGIN\n"
+                    + "	SELECT com.identificador from computadores com, datos  dt \n"
+                    + "	WHERE (fechaConsulta >= dt.logon and dt.logoff is NULL) AND com.equipo = dt.equipo;\n"
+                    + "END;");*/
             //String rs = " {call SP_GETEQUIPOSOCUPADOS('2016-08-10 18:19:03')}" ;
-            String rs = " {call SP_GETEQUIPOSOCUPADOS('"+annio+"-"+mes+"-"+dia+" "+hora1+":"+minutos1+":"+segundos1+"')}" ;
+            String rs = " {call SP_GETEQUIPOSOCUPADOS('" + annio + "-" + mes + "-" + dia + " " + hora1 + ":" + minutos1 + ":" + segundos1 + "')}";
             System.out.println(rs);
             System.out.println("cada 10 segundos");
             CallableStatement cSP = con.prepareCall(rs);
             ResultSet r = cSP.executeQuery();
-            
-            while(r.next()){
+
+            while (r.next()) {
                 System.out.println(r.getString(1));
                 equipos.add(r.getString(1));
             }
-            
+
         } catch (Exception ex) {
             Logger.getLogger(base.class.getName()).log(Level.SEVERE, null, ex);
         }
